ff5f207007f0d4f59eb96122c50607399e1ab458
Gazorby <gazorby@pm.me>
2020-04-04 15:41:36 +0200

    🐛 fix: rename fssh to match file name

--- functions/fssh.fish
+++ functions/fssh.fish
@@ -1,4 +1,4 @@
-function fdssh -d "Fuzzy-find ssh host via grep and ssh into it"
+function fssh -d "Fuzzy-find ssh host via grep and ssh into it"
     grep --ignore-case '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzf | read -l result
     and ssh "$result"
 end
