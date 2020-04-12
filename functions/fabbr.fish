8761267ddb749c3a6679f99dac338935f7f7bca9
Gazorby <gazorby@pm.me>
2020-04-13 01:47:26 +0200

    ♻️ refactor(fabbr): use more builtins

--- functions/fabbr.fish
+++ functions/fabbr.fish
@@ -1,5 +1,5 @@
 function fabbr --description "Find abbreviation and paste it to commandline"
     # We are using '²' character as a delimiter as it is not frequently used in commands
-    set ab (abbr | awk -F '-- ' '{print $2}' | sed -e 's/ /²/' | column -ts '²' | fzf -1 -0) \
-        && commandline (echo "$ab" | awk '{print $1}') || return 1
+    set -l ab (string replace ' ' '²' (string replace -r '.*-- ' '' (abbr -s)) | column -ts '²' | fzf -1 -0) \
+        && commandline (string match -r '\\w+' "$ab") || return 1
 end
