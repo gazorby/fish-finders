d33e10ff4eb3832d7c449f9374ff131da04ea41c
Gazorby <gazorby@pm.me>
2020-04-15 14:55:02 +0200

    ♻️ refactor: erase functions on uninstall

---
+++ conf.d/fish_finders.fish
@@ -0,0 +1,10 @@
+function __fish_finders_uninstall --on-event fish_finders_uninstall
+    functions --erase fbranch
+    functions --erase fabbr
+    functions --erase fenv
+    functions --erase fif
+    functions --erase fkill
+    functions --erase fpid
+    functions --erase fport
+    functions --erase fssh
+end
